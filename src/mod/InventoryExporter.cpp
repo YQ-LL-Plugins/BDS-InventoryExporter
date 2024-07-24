a6ed4ff1e823ee6ffcf168937b0de946907025b0
yqs112358 <yqs112358@outlook.com>
2024-07-24 20:38:01 +0800

    add partly export

--- src/mod/InventoryExporter.cpp
+++ src/mod/InventoryExporter.cpp
@@ -1,6 +1,7 @@
 #include "IncludeAll.h"
 #include "DBStorage.h"
 #include "ServerID.h"
+#include <filesystem>
 
 std::unique_ptr<CompoundTag> getOfflineNbt(std::string const& serverId) {
     if (!globalDBStorage->hasKey(serverId, DBHelpers::Category::Player)) {
@@ -25,20 +26,66 @@ std::unique_ptr<CompoundTag> getPlayerNbt(mce::UUID const& uuid) {
     }
 }
 
-bool savePlayerData(mce::UUID const& uuid) {
-    auto nbt = getPlayerNbt(uuid);
+bool savePlayerData(mce::UUID const& uuid, const std::string &playerName = "") {
+    auto playerNameId = playerName.empty() ? uuid.asString() : playerName;
+    logger.info("Exporting inventory for " + playerNameId + "...");
+
+    auto playerData = getPlayerNbt(uuid);
     ll::file_utils::writeFile(
-        "./plugins/InventoryExporter/save/" + uuid.asString() + ".dat",
-        nbt->toBinaryNbt(),
+        "./plugins/InventoryExporter/saved/" + playerNameId + "-PlayerData.nbt",
+        playerData->toBinaryNbt(),
         true
     );
-    return false;
+
+    auto inventoryList = playerData->getList("Inventory");
+    auto inventory = CompoundTag();
+    inventory.put("Inventory", inventoryList->copy());
+    ll::file_utils::writeFile(
+        "./plugins/InventoryExporter/saved/" + playerNameId + "-Inventory.nbt",
+        inventory.toBinaryNbt(),
+        true
+    );
+    inventory.clear();
+
+    auto armorList = playerData->getList("Armor");
+    auto armor = CompoundTag();
+    armor.put("Armor", armorList->copy());
+    ll::file_utils::writeFile(
+        "./plugins/InventoryExporter/saved/" + playerNameId + "-Armor.nbt",
+        armor.toBinaryNbt(),
+        true
+    );
+    armor.clear();
+
+    auto offhandList = playerData->getList("Offhand");
+    auto offhand = CompoundTag();
+    offhand.put("Offhand", offhandList->copy());
+    ll::file_utils::writeFile(
+        "./plugins/InventoryExporter/saved/" + playerNameId + "-Offhand.nbt",
+        offhand.toBinaryNbt(),
+        true
+    );
+    offhand.clear();
+
+    auto endChestList = playerData->getList("EnderChestInventory");
+    auto endChest = CompoundTag();
+    endChest.put("EnderChestInventory", endChestList->copy());
+    ll::file_utils::writeFile(
+        "./plugins/InventoryExporter/saved/" + playerNameId + "-EndChest.nbt",
+        endChest.toBinaryNbt(),
+        true
+    );
+    endChest.clear();
+
+    logger.info("Exported to ./plugins/InventoryExporter/saved/");
+    return true;
 }
 
 void exportInventories() {
-    std::string uuid = "9bcaac62-8e99-372f-b416-cb6f47130479";
+    // clean up
+    std::error_code ec;
+    std::filesystem::remove_all(std::filesystem::path("./plugins/InventoryExporter/saved"), ec);
 
-    logger.info("Exporting inventory for yqs112358...");
-    savePlayerData(uuid);
-    logger.info("Inventory exported");
+    std::string uuid = "9bcaac62-8e99-372f-b416-cb6f47130479";
+    savePlayerData(uuid, "yqs112358");
 }
